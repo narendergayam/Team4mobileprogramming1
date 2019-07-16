8ce67e47b0cc07fad213dc6bd910fe3589ac6b27
narendergayam <narendarreddyg16@gmail.com>
2019-07-15 21:26:13 -0400

    orderdet

---
+++ app/src/main/java/com/example/team4_mobileprogramming_1/orderdet.java
@@ -0,0 +1,41 @@
+package com.example.team4_mobileprogramming_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+public class orderdet extends AppCompatActivity {
+
+
+    ImageView o_img;
+    TextView o_name, o_followers, o_following, o_repo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_orderdet);
+
+        o_name = findViewById(R.id.o_name);
+        o_followers = findViewById(R.id.o_followers);
+        o_following = findViewById(R.id.o_following);
+        o_repo = findViewById(R.id.o_repo);
+        o_img = findViewById(R.id.o_img);
+
+        Intent intent = getIntent();
+        Repositories repositories = intent.getParcelableExtra("owner");
+        Picasso.get().load(repositories.getUseravatar()).into(o_img);
+
+        o_followers.setText(repositories.getFollowers());
+        o_following.setText(repositories.getFollowing());
+        o_name.setText(repositories.getUsername());
+        o_repo.setText(repositories.getUserrepository());
+
+
+
+    }
+}
